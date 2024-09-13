f6d165f6cb85a778a6cee858922971c34dd8acf9
lhh <larshalvorhansen1@gmail.com>
2024-09-13 12:22:53 +0200

    BEGIN Covariance calc

---
+++ relationalDatabase/calc.py
@@ -0,0 +1,30 @@
+import csv
+
+def convertToArray(csvFile):
+    with open(f"relationalDatabase/commoditiesData/{csvFile}.csv", newline='', encoding='utf-8') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)
+        myArray = []
+        for row in csvreader:
+            myArray.append(row)
+        return myArray
+
+def convertFromArrayWithToupleToList(array):
+    newArray
+    for i in array:
+         += 
+    return newArray
+
+def averageArray(array):
+    sum = 0
+    for i in array:
+        sum += float(i[1])
+    return sum / len(array)
+
+def main():
+    allComm = convertToArray("allCommodities")
+    print(averageArray(allComm))
+
+if __name__ == "__main__":
+    main()
+
